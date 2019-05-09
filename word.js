58b8b8aa551b67f3645b61f6ac40777a39b7fdbc
kbub27 <kalebmerriman27@gmail.com>
2019-05-08 23:41:16 -0700

    changed up the Word constructor but still not working correct

--- word.js
+++ word.js
@@ -1,27 +1,25 @@
 var Letter = require('./letter.js');
 
 class Word {
-    constructor(string) {
+    constructor(word) {
         //CREATE EMPTY ARRAY TO STORE LETTERS OF A WORD IN
-        this.array = [],
-        //SPLIT THE STRING AND PUSH EACH LETTER INTO THE ARRAY
-        string.split('').forEach(element => {
-            this.array.push(new Letter(element));
-        }),
+        this.letters = word.split("").map(function(char) {
+            return new Letter(char);
+          });
         //FUNCTION TO RETURN A STRING FOR THE WORD 
         this.toString = function () {
             return this.array.join(' ');
         },
         //A GUESS FUNCTION CALLING THE LETTER.JS GUESS FUNCTION
         this.guess = function (letterGuess) {
-            this.array.forEach(element => {
+            this.letters.forEach(element => {
                 element.guess(letterGuess);
             });
         },
         //CHECK IF ALL LETTERS ARE GUESSED 
         //DONT QUITE KNOW HOW THIS WORKS GOT IT FROM STACK OVERFLOW
         this.allDone = function () {
-            return this.array.every((currentValue) => currentValue.guessed);
+            return this.letters.every((currentValue) => currentValue.guessed);
         }
 
     }
